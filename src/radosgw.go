b5307a386464d1509a43e3f04a32cbaa39cc622a
wangjun <wangjun@yovole.com>
2018-11-27 18:09:58 +0800

    edit

--- src/radosgw.go
+++ src/radosgw.go
@@ -3,38 +3,58 @@
  */
 package radosgw
 
+import (
+	"github.com/astaxie/beego/httplib"
+)
+
 type Connection struct {
+	url   string
 	token string
 }
 
-func New(user string, key string, url string) (*Connection) {
+func New(user string, key string, auth_url string) *Connection {
+	b := httplib.Get(auth_url)
+	b.Header("X-Auth-User", user)
+	b.Header("X-Auth-Key", key)
+	res, err := b.DoRequest()
+	if err != nil {
+		return nil
+	}
+
+	return &Connection{res.Header.Get("x-storage-url"), res.Header.Get("x-storage-token")}
+}
+
+func (conn *Connection) Create_container(container string) {
 
 }
 
-func (con *Connection) create_container(container string) {
+func (conn *Connection) List_containers(v interface{}) error {
+	b := httplib.Get(conn.url)
+	b.Header("X-Auth-Token", conn.token)
+
+	return b.ToJSON(v)
+}
+
+func (conn *Connection) List_container_contents(container string, v interface{}) error {
+	url := conn.url + container
+	b := httplib.Get(url)
+	b.Header("X-Auth-Token", conn.token)
+
+	return b.ToJSON(v)
+}
+
+func (conn *Connection) Delete_container(container string) {
 
 }
 
-func (con *Connection) list_containers() (containers List){
+func (conn *Connection) Create_object(object string) {
 
 }
 
-func (con *Connection) list_container_contents(container string) (contents List){
+func (conn *Connection) Put_object(object []byte) {
 
 }
 
-func (con *Connection) delete_container(container string) {
-
-}
-
-func (con *Connection) create_object(object string){
-
-}
-
-func (con *Connection) put_object(object []byte){
-
-}
-
-func (con *Connection) delete_object(object string){
+func (conn *Connection) Delete_object(object string) {
 
 }
