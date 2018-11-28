d538cb42a3200643144093cd4cb0bb7a42baa5f2
wangjun <wangjun@yovole.com>
2018-11-28 16:26:17 +0800

    fix

--- src/radosgw.go
+++ src/radosgw.go
@@ -1,5 +1,5 @@
 /*
- * this package is trying to realize ceph         object client with go
+ * this package is trying to realize ceph's object client with go
  */
 package radosgw
 
@@ -25,7 +25,10 @@ func New(user string, key string, auth_url string) *Connection {
 }
 
 func (conn *Connection) Create_container(container string) {
-
+	url := conn.url + "/" + container
+	b := httplib.Put(url)
+	b.Header("X-Auth-Token", conn.token)
+	b.DoRequest()
 }
 
 func (conn *Connection) List_containers(v interface{}) error {
@@ -44,14 +47,27 @@ func (conn *Connection) List_container_contents(container string, v interface{})
 }
 
 func (conn *Connection) Delete_container(container string) {
+	url := conn.url + "/" + container
+	b := httplib.Delete(url)
+	b.Header("X-Auth-Token", conn.token)
 
+	b.DoRequest()
 }
 
 // Create or update an object
-func (conn *Connection) Put_object(container string, object string, object []byte) {
+func (conn *Connection) Put_object(container string, object string, data []byte) {
+	url := conn.url + "/" + container + "/" + object
+	b := httplib.Put(url)
+	b.Header("X-Auth-Token", conn.token)
+	b.Body(data)
 
+	b.DoRequest()
 }
 
-func (conn *Connection) Delete_object(object string) {
+func (conn *Connection) Delete_object(container string, object string) {
+	url := conn.url + "/" + container + "/" + object
+	b := httplib.Delete(url)
+	b.Header("X-Auth-Token", conn.token)
 
+	b.DoRequest()
 }
